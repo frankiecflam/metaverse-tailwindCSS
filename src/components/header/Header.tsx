2cbc28eb577537e7e2bf561b8dd5032c9e0eb5e1
Frankie Lam <frankie191193@gmail.com>
2022-12-20 13:38:04 +0000

    add a component covering half of the header with linear gradient bg image

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,8 +4,9 @@ import HeaderNavList from "./HeaderNavList";
 
 const Header = () => {
   return (
-    <header className="w-3/4 py-9 mx-auto">
-      <nav className="flex justify-between items-center">
+    <header className="relative">
+      <div className="absolute top-0 left-0 w-1/2 -z-50 bg-universe blur-[125px] h-full" />
+      <nav className="w-3/4 py-9 mx-auto flex justify-between items-center">
         <HeaderNavActions />
         <HeaderNavBrand />
         <HeaderNavList />
