da621fddd49c32c968688bcb7c194e8cbc8629ab
Frankie Lam <frankie191193@gmail.com>
2022-12-20 12:23:16 +0000

    add HeaderNavBrand

--- src/components/header/HeaderNavBrand.tsx
+++ src/components/header/HeaderNavBrand.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Link from "next/link";
 
 const HeaderNavBrand = () => {
-  return <div>HeaderNavBrand</div>;
+  return (
+    <div>
+      <Link href="/">
+        <h2 className="uppercase font-extrabold text-2xl leading-[30.24px]">
+          metaverse
+        </h2>
+      </Link>
+    </div>
+  );
 };
 
 export default HeaderNavBrand;
