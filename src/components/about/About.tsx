dd86359c41425f9212e790db9fc87082a90964c1
Frankie Lam <frankie191193@gmail.com>
2022-12-21 12:11:41 +0000

    replace text-white text-[arbitrary value] with custom colors from config

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -6,19 +6,21 @@ const About = () => {
     <section className="section relative" id="about">
       <div className="container mx-auto">
         <h1 className="section-heading">| About Metaverus</h1>
-        <p className="text-[24px] md:text-[36px] text-[#c7c7c7] font-medium text-center leading-[180%]">
-          <span className="text-white text-3xl font-extrabold">Metaverse</span>{" "}
+        <p className="text-[24px] md:text-[36px] text-secondary font-medium text-center leading-[180%]">
+          <span className="text-primary text-3xl font-extrabold">
+            Metaverse
+          </span>{" "}
           is a new thing in the future, where you can enjoy the virtual world by
           feeling like it&apos;s really real, you can feel what you feel in this
           metaverse world, because this is really the{" "}
-          <span className="text-white text-3xl font-extrabold">
+          <span className="text-primary text-3xl font-extrabold">
             madness of the metaverse
           </span>{" "}
           of today, using only{" "}
-          <span className="text-white text-3xl font-extrabold">VR</span> devices
-          you can easily explore the metaverse world you want, turn your dreams
-          into reality. Let&apos;s{" "}
-          <span className="text-white text-3xl font-extrabold">explore</span>{" "}
+          <span className="text-primary text-3xl font-extrabold">VR</span>{" "}
+          devices you can easily explore the metaverse world you want, turn your
+          dreams into reality. Let&apos;s{" "}
+          <span className="text-primary text-3xl font-extrabold">explore</span>{" "}
           the madness of the metaverse by scrolling down
         </p>
         <Image
