0d70c8390088598654940928723be6538f29bae9
Frankie Lam <frankie191193@gmail.com>
2022-12-21 15:37:56 +0000

    add a div for rendering overlay component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -7,7 +7,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <div
+          id="overlay-root"
+          className="fixed top-0 left-0 h-screen w-screen"
+        />
       </body>
     </Html>
-  )
+  );
 }
