33a80beae80a9e8762c602a15deaf9ee410c5f11
Frankie Lam <frankie191193@gmail.com>
2022-12-21 12:01:55 +0000

    refactor css styles from section-subHeading into a reusable class - section-subHeading

--- src/components/explore/Explore.tsx
+++ src/components/explore/Explore.tsx
@@ -6,7 +6,7 @@ const Explore = () => {
       <div className="bg-[#9542E859] blur-[125px] w-[504px] h-[1236.81px] absolute -rotate-[114.2deg] -top-[150%]" />
       <div className="container mx-auto">
         <h1 className="section-heading">| The World</h1>
-        <p className="max-w-[885px] mx-auto font-bold text-[32px] leading-[40.32px] sm:text-[48px] lg:text-[64px] sm:leading-[80.64px] tracking-tight text-center">
+        <p className="section-subHeading">
           Choose the world you want to explore
         </p>
         <ExploreFeatureList />
