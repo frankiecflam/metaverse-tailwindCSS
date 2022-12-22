3a279439ca5953b0c8653aa46447d018c1cf9258
Frankie Lam <frankie191193@gmail.com>
2022-12-22 11:03:21 +0000

    add Insights section

---
+++ src/components/insights/Insights.tsx
@@ -0,0 +1,15 @@
+import InsightList from "./InsightList";
+
+const Insights = () => {
+  return (
+    <section className="section">
+      <div className="container mx-auto">
+        <h1 className="section-heading">| Insight</h1>
+        <p className="section-subHeading">Insight about metaverse</p>
+        <InsightList />
+      </div>
+    </section>
+  );
+};
+
+export default Insights;
