c669914139a0c19c66aaf4c492cde82f2b1a3e99
Frankie Lam <frankie191193@gmail.com>
2022-12-23 10:53:41 +0000

    change the text-direction of section heading to text-start

--- src/components/getStarted/GetStarted.tsx
+++ src/components/getStarted/GetStarted.tsx
@@ -15,7 +15,7 @@ const GetStarted = () => {
           <div className="bg-[#2d4898bf] blur-[200px] top-[10%] left-[30%] -rotate-[53.13deg] w-[304px] h-screen absolute -z-50" />
         </div>
         <div>
-          <h1 className="section-heading">| How Metaverus Works</h1>
+          <h1 className="section-heading text-start">| How Metaverus Works</h1>
           <p className="section-subHeading text-start">
             Get started with just a few clicks
           </p>
